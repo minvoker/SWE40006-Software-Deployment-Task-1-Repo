a4d97f4a1d285947254dfd5993361af2e09909ae
Max <maxharro1337@gmail.com>
2025-08-24 00:38:52 +1000

    Add src and wix files manually via upload for each task

---
+++ Task1.3_DllDemo/SWE40006.MathLib/MathOps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWE40006.MathLib
+{
+    public static class MathOps // basic arithmetic
+    {
+        public static long Add(long a, long b) { return a + b; }
+        public static long Multiply(long a, long b) { return a * b; }
+    }
+}
