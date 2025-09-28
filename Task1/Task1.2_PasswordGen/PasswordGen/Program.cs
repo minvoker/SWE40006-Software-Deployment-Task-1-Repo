17b3b4504eba17abb85d325d49edba90cafbfc32
minvoker <maxharro1337@gmail.com>
2025-09-28 18:15:24 +1000

    Dir restructure

---
+++ Task1/Task1.2_PasswordGen/PasswordGen/Program.cs
@@ -0,0 +1,17 @@
+﻿using PasswordGen;
+using System;
+using System.Windows.Forms;
+
+namespace BasicPasswordGen
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
